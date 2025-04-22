512d4e26fdcfcfa7105ca017eefb515cbc98de3c
Anle-He <sicheng_he@163.com>
2025-04-22 21:57:20 +0800

    fea: add main execution script for DSTMamba and update README to reference new script

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-python exp_main.py -m DSTMamba -t LTSF -d PEMS08 -cfg ../baselines/DSTMamba/LTSFConfig/PEMS08_IN96_OUT12.yaml
+bash exp/exp_ltsf_pems08.sh
---
+++ exp/exp_ltsf_pems08.sh
@@ -0,0 +1,4 @@
+python main.py -m DSTMamba -t LTSF -d PEMS08 -cfg ../baselines/DSTMamba/LTSFConfig/PEMS08_IN96_OUT12.yaml
+python main.py -m DSTMamba -t LTSF -d PEMS08 -cfg ../baselines/DSTMamba/LTSFConfig/PEMS08_IN96_OUT24.yaml
+python main.py -m DSTMamba -t LTSF -d PEMS08 -cfg ../baselines/DSTMamba/LTSFConfig/PEMS08_IN96_OUT48.yaml
+python main.py -m DSTMamba -t LTSF -d PEMS08 -cfg ../baselines/DSTMamba/LTSFConfig/PEMS08_IN96_OUT96.yaml
